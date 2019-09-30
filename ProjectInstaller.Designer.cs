df57387a706e162bc922b9ca0b33a1bcee31d4ac
GetRaid <32432718+getraid@users.noreply.github.com>
2019-09-30 03:11:16 +0200

    Init

---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DesktopKillSwitch_Client
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dkscServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.dkscServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // dkscServiceProcessInstaller
+            // 
+            this.dkscServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.dkscServiceProcessInstaller.Password = null;
+            this.dkscServiceProcessInstaller.Username = null;
+            // 
+            // dkscServiceInstaller
+            // 
+            this.dkscServiceInstaller.Description = "Sends a signal once windows gets shutdown";
+            this.dkscServiceInstaller.DisplayName = "Desktop Kill Switch (Remote Node.js)";
+            this.dkscServiceInstaller.ServiceName = "Service1";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.dkscServiceProcessInstaller,
+            this.dkscServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller dkscServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller dkscServiceInstaller;
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopKillSwitch_Client
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        // https://stackoverflow.com/a/3808841
+        private bool RemoteFileExists(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //check if settings exist
+            if (Properties.Settings.Default.ipOfNodeServer != null && Properties.Settings.Default.port != null)
+            {
+                // apply ip. If can't reach ip:port/desktopkillswitch,
+                if (RemoteFileExists(string.Concat(@"http//{0}:{1}/desktopkillswitch", Properties.Settings.Default.ipOfNodeServer, Properties.Settings.Default.port)))
+                {
+                }
+                else
+                {
+                    // ip is invaild and service can stop
+                    Stop();
+                }
+            }
+            else
+            {
+                // save empty dummy settings and restart
+                // OnStart(null);
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+        protected override void OnShutdown()
+        {
+            //Here insert code to connect to nodeserver and send signal.
+            base.OnShutdown();
+        }
+    }
+}
