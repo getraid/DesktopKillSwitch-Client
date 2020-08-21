f5550486fe674de6e3ec26b96d3b6ff8532e1b77
getraid <32432718+getraid@users.noreply.github.com>
2020-08-21 19:35:23 +0200

    Installer update,Ip from Registry,Regex validation

--- DesktopKillSwitch-Client/DKSC.cs
+++ DesktopKillSwitch-Client/DKSC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,48 +12,49 @@ using System.Net.Http.Headers;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 
 namespace DesktopKillSwitch_Client
 {
     public partial class DKSC : ServiceBase
     {
+
+
         public DKSC()
         {
             InitializeComponent();
-            CanShutdown = true;
-        }
 
-        public string KillServer { get; set; } = "http://192.168.178.45:5123";
+        }
+        public string Apistring { get; set; }
 
         private static readonly HttpClient client = new HttpClient();
 
-        protected override void OnStart(string[] args)
+        protected override void OnStart(String[] args)
         {
-            //load ip from text file 'ip'
-            try
+
+            var api = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\getraid", "DESKTOPKILLSWITCHAPI", "-1");
+            var port = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\getraid", "DESKTOPKILLSWITCHPORT", "-1");
+
+            //Regex to verify that the value of the user is 
+            string ipPattern = @"^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$";
+            bool validIp = Regex.Match(api, ipPattern).Success;
+
+            string portPattern = @"^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$";
+            bool validPort = Regex.Match(port, portPattern).Success;
+
+            if (validIp && validPort)
             {
-                //doesnt work. Permissions maybe? rn hardcode is the way :^)
-                KillServer = System.IO.File.ReadAllText(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"ip"));
+                var combined = "http://" + api + ":" + port;
+                Apistring = combined;
             }
-            catch
+            else
             {
-           
+                EventLog.WriteEntry("DesktopKillSwitch", "IP or PORT are wrong / not entered. Please reinstall the program and use a valid IP and PORT for the server. " +
+                 @"If you don't want to reinstall, change the settings in the Windows Registry at 'HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\getraid' ", EventLogEntryType.Error);
+                Stop();
             }
 
-        }
-
-        protected override void OnStop()
-        {
-            //test
-            //try
-            //{
-            //    client.PostAsync(KillServer + "/shutdown", null).Wait();
-            //}
-            //catch (Exception)
-            //{
-            //    throw;
-            //}
+            base.OnStart(args);
         }
 
         /// <summary>
@@ -60,7 +62,8 @@ namespace DesktopKillSwitch_Client
         /// </summary>
         protected override void OnShutdown()
         {
-            client.PostAsync(KillServer + "/shutdown", null).Wait();
+
+            client.PostAsync(Apistring + "/shutdown", null).Wait(30000);
             base.OnShutdown();
         }
     }
